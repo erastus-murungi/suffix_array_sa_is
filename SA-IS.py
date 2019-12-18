668c2e1dc1d603c0bae5274d56f6da0a9e7f98e2
Erastus11013 <erastuswagwan@gmail.com>
2019-12-18 00:25:46 -0500

    Added testing

--- SA-IS.py
+++ SA-IS.py
@@ -1,19 +1,21 @@
 from array import array
 
+
+__author__ = 'Erastus Murungi'
+
+
 L_TYPE = ord('L')
 S_TYPE = ord('S')
 SIGMA = 256
 
 
 def is_lms(i, t):
+    """Returns whether the suffix/ character at index i is a leftmost S-type"""
     return t[i] == S_TYPE and t[i - 1] == L_TYPE
 
 
-def ranks(text, n):
-    pass
-
-
 def print_(data: bytearray):
+    """Simple method to the types of the characters of T"""
     print(data.decode('ascii'))
     print("".join(
         "^" if is_lms(i, data) else " "
@@ -21,7 +23,16 @@ def print_(data: bytearray):
     ))
 
 
-def classify(text, n):
+def classify(text, n) -> bytearray:
+    """Classifies the suffixes in text as either S-type, or L-type
+    This method can be merged with find_lms_suffixes but I have not done so for readability
+    Args:
+        text: the input string/array to be classified
+        n: the length of text
+    Returns:
+        t: a bytearray object, where t[i] contains the type of text[i]
+
+    """
     t = bytearray(n)
     t[-1] = S_TYPE
     for i in range(n - 2, -1, -1):
@@ -36,6 +47,11 @@ def classify(text, n):
 
 
 def find_lms_suffixes(t, n):
+    """Finds the positions of all lms_suffixes
+    Args:
+        t: the type array
+        n: the length of text and t
+    """
     pos = array('l')
     for i in range(n - 1, 0, -1):
         if t[i] == S_TYPE and t[i - 1] == L_TYPE:
@@ -44,6 +60,7 @@ def find_lms_suffixes(t, n):
 
 
 def print_buckets(bucks):
+    """Simple method to print bucket sizes"""
     res = '[ '
     for b in bucks:
         if b != 0:
@@ -54,6 +71,7 @@ def print_buckets(bucks):
 
 
 def buckets(text):
+    """Find the alphabet and the sizes of the bucket for each character in the text"""
     alpha = bytearray()
     bucket_sizes = array('L', [0] * SIGMA)
     for c in text:
@@ -68,6 +86,7 @@ def buckets(text):
 
 
 def bucket_intervals(alpha, bucket_sizes):
+    """Computes the bucket intervals, i.e heads and tails"""
     heads = array('l', [0] * SIGMA)
     tails = array('l', [0] * SIGMA)
     j = 0
@@ -82,6 +101,19 @@ def bucket_intervals(alpha, bucket_sizes):
 
 
 def induced_sorting(lms, tails, heads, SA, type_suffix, text, n, alpha, bucket_sizes):
+    """Inductively creates the suffix array based on LMS
+    Args:
+        lms: an array indicating the positions of LMS Blocks/Suffixes in text
+        tails: an array indexed by the characters in T which tells the ends of the buckets
+        heads: an array indexed by the characters in T which tells the fronts of the buckets of those characters
+        SA: an empty array to be filled during the creation of the suffix array
+        type_suffix: an array in which type_suffix[i] tells the type of text[i]
+        text: the input whose suffix array is to be created
+        n: the length of the input 'text'
+        alpha: an array of the alphabet of T in sorted order
+        bucket_sizes: an array containing the sizes of each bucket: Used in resetting heads, tails
+
+        """
     for s in lms:  # place LMS suffixes at the end of their buckets
         nfs = tails[text[s]]
         SA[nfs] = s
@@ -104,6 +136,7 @@ def induced_sorting(lms, tails, heads, SA, type_suffix, text, n, alpha, bucket_s
 
 
 def blocks_are_equal(i, j, types, text, n):
+    """Testing for teh equality of two blocks"""
     while i < n and j < n:
         if text[i] == text[j]:
             if is_lms(i, types) and is_lms(j, types):
@@ -117,6 +150,7 @@ def blocks_are_equal(i, j, types, text, n):
 
 
 def get_reduced_substring(types, SA, lms, ordered_lms, text, n, m):
+    """Finds the reduced substring"""
     j = 0
     for i in range(n):
         if is_lms(SA[i], types):
@@ -143,47 +177,53 @@ def get_reduced_substring(types, SA, lms, ordered_lms, text, n, m):
         pIS[i] = ipIS[text[lms[i]]]
 
     pIS.reverse()
-    print(pIS)
     return pIS, k == m
 
 
-def suffix_array(T, SA):
-    n = len(T)
-    t = classify(T, n)
+def suffix_array(T, SA, n):
+    """Main method"""
 
-    lms = find_lms_suffixes(t, n)
+    t = classify(T, n)  # step 1: classification
+    lms = find_lms_suffixes(t, n)  # step 2: finding the LMS suffixes
     m = len(lms)
 
     print_(t)
 
-    alpha, sizes = buckets(T)
-
+    alpha, sizes = buckets(T)  # finding the bucket sizes and alphabet of T
     heads, tails = bucket_intervals(alpha, sizes)
     induced_sorting(lms, tails, heads, SA, t, T, n, alpha, sizes)   # first induced sort
 
     ordered_lms = array('L', [0] * len(lms))
 
-    reduced_ST, blocks_unique = get_reduced_substring(t, SA, lms, ordered_lms, T, n, m)
-    SA_reduced = array('l', [-1] * m)
+    reduced_text, blocks_unique = get_reduced_substring(t, SA, lms, ordered_lms, T, n, m)
+    reduced_SA = array('l', [-1] * m)  # reduced SA
     if blocks_unique:  # base case
         # compute suffix array manually
         for i in range(m):
-            SA_reduced[reduced_ST[i]] = i
+            reduced_SA[reduced_text[i]] = i
     else:
-        suffix_array(reduced_ST, SA_reduced)
+        suffix_array(reduced_text, reduced_SA, m)
         # use the suffix array to sort the LMS suffixes
+        lms.reverse()
         for i in range(m):
-            lms[i] = ordered_lms[SA_reduced[i]]
+            ordered_lms[i] = lms[reduced_SA[i]]
 
+        heads, tails = bucket_intervals(alpha, sizes)  # reset counters
+        for i in range(n):  # clear suffix array
+            SA[i] = 0
         induced_sorting(lms, tails, heads, SA, t, T, n, alpha, sizes)
 
 
-if __name__ == '__main__':
-    T = 'GTCCCGATGTCATGTCAGGA'
+def test_SA(T):
     T += '$'
     T = [ord(c) for c in T]
     n = len(T)
     SA = array('l', [-1] * n)
 
-    suffix_array(T, SA)
+    suffix_array(T, SA, n)
+    return SA
 
+
+if __name__ == '__main__':
+    T = 'ACGTGCCTAGCCTACCGTGCC'
+    SA = test_SA(T)
