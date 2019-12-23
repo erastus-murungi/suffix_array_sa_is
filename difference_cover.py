7321e16d70ffbb49f05f53d89470c5346127e086
Erastus11013 <erastuswagwan@gmail.com>
2019-12-22 21:23:33 -0500

    Update difference_cover.py

--- difference_cover.py
+++ difference_cover.py
@@ -4,12 +4,14 @@ from array import array
 
 
 def construct_suffix_array(s):
-    n = len(s)
-    s += (chr(0) * 3)
+    n = len(s) + 1
+    s += (chr(36) * 4)
+
     SA = array('l', [0 for _ in s])
     alpha = sorted(set(s))
     difference_cover(s, SA, n, alpha)
     return SA[:-3]
+    # return array('l', [n]) + SA[:-3]
 
 
 def difference_cover(s, SA, n, alpha):
@@ -19,31 +21,35 @@ def difference_cover(s, SA, n, alpha):
     n02 = n0 + n2
     SA12 = [0] * (n02 + 3)
     SA0 = [0] * n0
-    s12 = [i for i in range(n + n0 - n1) if i % 3 != 0]
+    s12 = [i for i in range(n + (n0 - n1)) if i % 3 != 0]
+    # substrings = sorted([s[i: i + 3] for i in s12])
+    # print(substrings)
 
     s12.extend([0, 0, 0])
 
-    radixpass(s12, SA12, s[2:], n02, alpha)
-    radixpass(SA12, s12, s[1:], n02, alpha)
-    radixpass(s12, SA12, s, n02, alpha)
+    radixpass(s12, SA12, s[2:], n02, alpha)   # sort using third character .. s + 2
+    radixpass(SA12, s12, s[1:], n02, alpha)  # sort using second character .. s + 1
+    radixpass(s12, SA12, s, n02, alpha)  # sort using first character .. s
 
-    #  merging step
-    name = 0
+    rank = 0
     c0, c1, c2 = -1, -1, -1
-    arr = [0]
+    arr = [0]  # rank of first suffix is 0
     for i in range(n02):
         if s[SA12[i]] != c0 or s[SA12[i] + 1] != c1 or s[SA12[i] + 2] != c2:
-            name += 1
-            arr.append(name)
+            rank += 1
+            arr.append(rank)
             c0 = s[SA12[i]]
             c1 = s[SA12[i] + 1]
             c2 = s[SA12[i] + 2]
-        if SA12[i] % 3 == 1:
-            s12[SA12[i] // 3] = name
-        else:
-            s12[SA12[i] // 3 + n0] = name
 
-    if name < n02:
+        if SA12[i] % 3 == 1:
+            s12[SA12[i] // 3] = rank
+
+        else:  # SA12[i] % 3 == 2
+            s12[SA12[i] // 3 + n0] = rank
+
+    # if the ranks are not unique, recurse on string s12, with SA=SA12, and alphabet = arr
+    if rank < n02:
         difference_cover(s12, SA12, n02, arr)
         for i in range(n02):
             s12[SA12[i]] = i + 1
@@ -53,9 +59,10 @@ def difference_cover(s, SA, n, alpha):
 
     s0 = [SA12[i] * 3 for i in range(n02) if SA12[i] < n0]
 
+    #  stably sort the mod 0 suffixes from SA12 by their first character
     radixpass(s0, SA0, s, n0, alpha)
 
-    p = j = k = 0
+    p = k = 0
     t = n0 - n1
     while k < n:
         i = SA12[t] * 3 + 1 if SA12[t] < n0 else (SA12[t] - n0) * 3 + 2
@@ -94,6 +101,7 @@ def difference_cover(s, SA, n, alpha):
 
 
 def radixpass(a, b, text, n, alpha):
+    """The text is shifted by i = 2, 1, 0"""
     c = {}    # an array can be used
     for char in alpha:
         c[char] = 0
@@ -115,8 +123,8 @@ def radixpass(a, b, text, n, alpha):
 
 def lcp_kasai(s, suffix_array):
     n = len(s)
-    rank = array('i', [0 for _ in range(n)])
-    LCP = array('i', [0 for _ in range(n)])
+    rank = array('i', [0 for _ in range(n + 1)])
+    LCP = array('i', [0 for _ in range(n + 1)])
     for i in range(n):
         rank[suffix_array[i]] = i
     h = 0
@@ -134,5 +142,7 @@ def lcp_kasai(s, suffix_array):
 
 
 if __name__ == '__main__':
-    s = construct_suffix_array('GTCCCGATGTCATGTCAGGA')
-    print(s)
+    t = 'yabbadabbadoo'
+    sa = construct_suffix_array(t)
+    lcp = lcp_kasai(t, sa)
+    print(sa, lcp)
